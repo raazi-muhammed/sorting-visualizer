fa49d436f1bb95ae869b8cb066d7b33089f55297
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-11-20 21:10:22 +0530

    Addded animations

--- sorting-visualizer/src/components/sortingVisualizer/SortingVisualizer.tsx
+++ sorting-visualizer/src/components/sortingVisualizer/SortingVisualizer.tsx
@@ -6,6 +6,7 @@ import SortIcon from "../../assets/icons/SortIcon";
 const SortingVisualizer = () => {
 	const [arrayValues, setArrayValues] = useState<number[]>([]);
 	const [comparing, setComparing] = useState<number[]>([]);
+	const [switching, setSwitching] = useState<number[]>([]);
 	const [delay, setDelay] = useState(20);
 	const [arrayLength, setArrayLength] = useState(25);
 
@@ -21,6 +22,7 @@ const SortingVisualizer = () => {
 		}
 		setArrayValues(randomArr);
 		setComparing([]);
+		setSwitching([]);
 	};
 	useEffect(() => {
 		randomizeArray();
@@ -33,8 +35,11 @@ const SortingVisualizer = () => {
 			do {
 				isItemsSwapped = false;
 				for (let i = 0; i < count; i++) {
+					setSwitching([]);
 					if (arr[i] > arr[i + 1]) {
 						isItemsSwapped = true;
+						setSwitching([i, i + 1]);
+						await new Promise((resolve) => setTimeout(resolve, delay / 2));
 						let temp = arr[i];
 						arr[i] = arr[i + 1];
 						arr[i + 1] = temp;
@@ -48,6 +53,7 @@ const SortingVisualizer = () => {
 
 			setArrayValues(arr);
 			setComparing([]);
+			setSwitching([]);
 			return arr;
 		}; // O(n^2)
 
@@ -57,7 +63,11 @@ const SortingVisualizer = () => {
 	return (
 		<main>
 			<section className="top-section">
-				<button onClick={handleBubbleSortArray} className="btn">
+				<button
+					onClick={() => {
+						handleBubbleSortArray();
+					}}
+					className="btn">
 					<SortIcon />
 					Bubble Sort
 				</button>
@@ -71,15 +81,24 @@ const SortingVisualizer = () => {
 						key={i}
 						style={{ height: `${val / 10}%` }}
 						className={`graph--bar ${
-							comparing.includes(i) ? "bar--accent" : ""
+							switching.includes(i)
+								? "bar--danger"
+								: comparing.includes(i)
+								? "bar--accent"
+								: ""
 						}`}>
 						<p className="graph--text">{val}</p>
 					</div>
 				))}
 			</section>
 			<aside style={{ position: "absolute", right: "2rem", top: "2rem" }}>
-				<div className="slide-container">
-					<p>{delay}</p>
+				<section>
+					<input
+						value={delay}
+						onChange={(e) => setDelay(Number(e.target.value))}
+						type="number"
+					/>
+					<br />
 					<input
 						min={1}
 						max={1000}
@@ -88,7 +107,14 @@ const SortingVisualizer = () => {
 						type="range"
 						className="slider"
 					/>
-					<p>{arrayLength}</p>
+				</section>
+				<section>
+					<input
+						value={arrayLength}
+						onChange={(e) => setArrayLength(Number(e.target.value))}
+						type="number"
+					/>
+					<br />
 					<input
 						min={5}
 						max={100}
@@ -97,7 +123,7 @@ const SortingVisualizer = () => {
 						type="range"
 						className="slider"
 					/>
-				</div>
+				</section>
 			</aside>
 		</main>
 	);
